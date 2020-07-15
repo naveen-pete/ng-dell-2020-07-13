27b30254adc06e513425a5efaff72f19cce444bc
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-07-15 11:01:16 +0530

    demo on pipes and directives

--- apps/day-03/store-app/src/app/products/products.component.ts
+++ apps/day-03/store-app/src/app/products/products.component.ts
@@ -1,26 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductModel } from '../models/product.model';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  productName: string = 'iPhone X';
-  productPrice: number = 50000;
-  productIsAvailable: boolean = true;
+  today = Date.now();
+
+  // product: ProductModel = new ProductModel();
+
+  // product: ProductModel = new ProductModel(1, 'iPhone XR', 'Smart phone from Apple', 65000, true);
+
+  // product: ProductModel = {
+  //   id: 1,
+  //   name: 'iPhone XR',
+  //   description: 'Smart phone from Apple',
+  //   price: 65000,
+  //   isAvailable: true
+  // };
+
+  products: ProductModel[] = [
+    {
+      id: 1,
+      name: 'iPhone X',
+      description: 'A mobile phone from Apple',
+      isAvailable: true,
+      price: 60000
+    },
+    {
+      id: 2,
+      name: 'Samsung Galaxy Note 10',
+      description: 'A mobile phone from Samsung',
+      isAvailable: true,
+      price: 80000
+    },
+    {
+      id: 3,
+      name: 'Google Pixel 3',
+      description: 'A mobile phone from Google',
+      isAvailable: false,
+      price: 50000
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onSave() {
-    console.log('Product saved.');
-  }
-
-  onSearch(event) {
-    console.log('You entered:', event.target.value);
   }
 
 }
